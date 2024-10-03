6a6d447e79027151d1f30ed9333b9702ad694e2d
Rajiya Sultana <rssumu04@gmail.com>
2024-10-03 12:46:07 +0600

    UI

---
+++ OceanQuest/Assets/Scripts/Pause Menu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
